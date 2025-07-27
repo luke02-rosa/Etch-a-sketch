f68e5c915b3480e88f568b40138d080c90d26090
luke <lukeerosa@gmail.com>
2025-07-27 11:16:42 +0200

    modified js with the pencil event

--- js.js
+++ js.js
@@ -2,19 +2,34 @@
 const contenitore = document.querySelector("#contenitore");
 //creazione 16x16 quadrati
 
+let mousePressed = false;
+
 for(let i = 0; i< 16*16; i++){
     const quadrato = document.createElement("div");
     quadrato.classList.add("quadrato");
     contenitore.appendChild(quadrato);
+    
 
-}
+    
+    quadrato.addEventListener("mousedown", () =>{
+         mousePressed = true;
+         
+    })
+     quadrato.addEventListener("mouseup", () =>{
+       mousePressed = false;
+    })
+
+    quadrato.addEventListener("mouseenter", () =>{
+        if(mousePressed === true){
+        quadrato.style.cssText = "background:yellow"
+    }
+    })
+   
+
+   
 
 
+     
 
-/*const div = document.createElement("div");
-const p = document.createElement("p");
-p.textContent = "ciao";
-p.style.cssText = "background:blue";
-div.appendChild(p);
-contenitore.appendChild(div);
-console.log(contenitore)*/
+   }
+
