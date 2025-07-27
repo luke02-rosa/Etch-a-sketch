494bd20bf11d5c4d9b7dad4d11e8d7d066a107a7
luke <lukeerosa@gmail.com>
2025-07-27 15:29:35 +0200

    rgb color added and others buttons

--- js.js
+++ js.js
@@ -4,13 +4,35 @@ const contenitore = document.querySelector("#contenitore");
 
 let mousePressed = false;
 
-for(let i = 0; i< 16*16; i++){
+const btngriglia = document.querySelector(".genera");
+let input = document.querySelector(".input");
+
+
+btngriglia.addEventListener("click", () => {
+  const answer = parseInt(input.value);
+ 
+  
+
+   
+
+if(answer<2 ||answer >50){
+    alert("numero non valido")
+} 
+
+contenitore.innerHTML = "";
+
+
+
+for(let i = 0; i< answer*answer; i++){
     const quadrato = document.createElement("div");
     quadrato.classList.add("quadrato");
+    quadrato.style.cssText = 
+    "width:" +(600/answer)+ "px; height:" +(600/answer)+ "px;"
     contenitore.appendChild(quadrato);
-    
+ 
 
-    
+    let Cursore = "red"
+
     quadrato.addEventListener("mousedown", () =>{
          mousePressed = true;
          
@@ -21,15 +43,46 @@ for(let i = 0; i< 16*16; i++){
 
     quadrato.addEventListener("mouseenter", () =>{
         if(mousePressed === true){
-        quadrato.style.cssText = "background:yellow"
+        quadrato.style.backgroundColor = Cursore
     }
     })
+    const button1 = document.querySelector(".button1")
+   const button2 = document.querySelector(".button2")
+   const button3 = document.querySelector(".button3")
+   const button4 = document.querySelector(".button4")
+   const reset = document.querySelector(".button5")
+
+   button1.addEventListener("click",()=>{
+    Cursore = "black"
+
+   })
+   button4.addEventListener("click",()=>{
+    Cursore = "white"
+
+   })
+   button2.addEventListener("input",()=>{
+    Cursore = button2.value;
+
+   })
+    button3.addEventListener("click",()=>{
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+
+    Cursore = `rgb(${r}, ${g}, ${b})`;
+
+   })
+    reset.addEventListener("click",()=>{
+    quadrato.style.backgroundColor = "white";
+
+   })
    
-
-   
-
-
-     
 
    }
+   }
+);
+
+
+
+   
 
